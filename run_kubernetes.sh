ea93aafd41c678b1457a778f600611af1b55a558
Elwy <Ennovation.Apps@gmail.com>
2021-10-10 16:37:36 +0200

    Deploy with Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="elwy/ml"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment elwy-ml --image=$dockerpath 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment/elwy-ml --type="NodePort" --port=5001 --target-port=8080
